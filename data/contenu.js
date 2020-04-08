b03b88391be731427dab9d464298d0a416111621
dulymboi <cynthia.dulymbois@etu.u-pec.fr>
2020-04-08 17:40:53 +0200

    commit

---
+++ data/contenu.js
@@ -0,0 +1,11 @@
+contenu : [
+					{
+						date : "07-04-2020",
+						resume : "suite à une discution avec le gérant de l'écurie, il se trouve que ses fils lui ont amené des chevaux volés dans le but de les revendre. ainsi, nos héros décident de se rendre au ranch des dits fils pour, je cite, \"leur casser la gueule\""
+					},
+
+					{
+						date : "05-04-2020",
+						resume : "Noct, Oram et Gwen se retrouvent à aider les gardiens après avoir vaincu un Sheikan affaiblit. Nul ne sais ce qu'il est devenu ni ou il est aller, la prudence est donc de mise. C'est dans ce climat ou nos héros ont décidé de visiter une ville voisine pas très honête... Ou Oram c'est fait détroussé. Nos heros essayent de recuperer les biens d'Oram et leurs chevaux, retrouvé dans une ecurie de la dit ville après s'etre fait magnipulé mentalement dans une église ((feat les fotes d'aurtaugrafe))"
+					}
+				]
