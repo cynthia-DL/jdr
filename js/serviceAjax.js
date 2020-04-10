da4bb394d75f5a98250c38922e83532f617b8ade
dulymboi <cynthia.dulymbois@etu.u-pec.fr>
2020-04-10 23:20:16 +0200

    on a toutes les requetes pour recuperer des enums

--- js/serviceAjax.js
+++ js/serviceAjax.js
@@ -2,10 +2,17 @@ function makeServiceAjax(){
 	var service = {
       getStoryLine:getStoryLine,
       getPersonnage:getPersonnage,
+      getAllAnimal:getAllAnimal,
+      getAllClasse:getAllClasse,
+      getAllEtat:getAllEtat,
+      getAllGenre:getAllGenre,
+      getAllRace:getAllRace,
+      getAllTypeInventaire:getAllTypeInventaire,
+      getAllTypeObjet:getAllTypeObjet
     };
 
     function getStoryLine() {
-        var url = "./php/get.php?case=story_line";
+        var url = "./php/get.php?case=storyline";
         return new Promise(function(resolve,reject){
             http = new XMLHttpRequest();
             http.open("GET",url);
@@ -34,6 +41,125 @@ function makeServiceAjax(){
             };
             http.onerror = function(){
                 reject("Erreur getPersonnage 2 : "+http.serverResponse);
+            };
+        });
+    }
+
+    function getAllAnimal() {
+        var url = "./php/get.php?case=all_animal";
+        return new Promise(function(resolve,reject){
+            http = new XMLHttpRequest();
+            http.open("GET",url);
+            http.send();
+            http.onload=function(){
+                if (http.status == 200){
+                    resolve(this.response);
+                } else reject("Erreur getAllAnimal 1 : "+http.serverResponse);
+            };
+            http.onerror = function(){
+                reject("Erreur getAllAnimal 2 : "+http.serverResponse);
+            };
+        });
+    }
+
+    function getAllClasse() {
+        var url = "./php/get.php?case=all_classe";
+        return new Promise(function(resolve,reject){
+            http = new XMLHttpRequest();
+            http.open("GET",url);
+            http.send();
+            http.onload=function(){
+                if (http.status == 200){
+                    resolve(this.response);
+                } else reject("Erreur getAllClasse 1 : "+http.serverResponse);
+            };
+            http.onerror = function(){
+                reject("Erreur getAllClasse 2 : "+http.serverResponse);
+            };
+        });
+    }
+
+    function getAllEtat() {
+        var url = "./php/get.php?case=all_etat";
+        return new Promise(function(resolve,reject){
+            http = new XMLHttpRequest();
+            http.open("GET",url);
+            http.send();
+            http.onload=function(){
+                if (http.status == 200){
+                    resolve(this.response);
+                } else reject("Erreur getAllEtat 1 : "+http.serverResponse);
+            };
+            http.onerror = function(){
+                reject("Erreur getAllEtat 2 : "+http.serverResponse);
+            };
+        });
+    }
+
+    function getAllGenre() {
+        var url = "./php/get.php?case=all_genre";
+        return new Promise(function(resolve,reject){
+            http = new XMLHttpRequest();
+            http.open("GET",url);
+            http.send();
+            http.onload=function(){
+                if (http.status == 200){
+                    resolve(this.response);
+                } else reject("Erreur getAllGenre 1 : "+http.serverResponse);
+            };
+            http.onerror = function(){
+                reject("Erreur getAllGenre 2 : "+http.serverResponse);
+            };
+        });
+    }
+
+    function getAllRace() {
+        var url = "./php/get.php?case=all_race";
+        return new Promise(function(resolve,reject){
+            http = new XMLHttpRequest();
+            http.open("GET",url);
+            http.send();
+            http.onload=function(){
+                if (http.status == 200){
+                    resolve(this.response);
+                } else reject("Erreur getAllRace 1 : "+http.serverResponse);
+            };
+            http.onerror = function(){
+                reject("Erreur getAllRace 2 : "+http.serverResponse);
+            };
+        });
+    }
+
+    function getAllTypeInventaire() {
+        var url = "./php/get.php?case=all_type_inventaire";
+        return new Promise(function(resolve,reject){
+            http = new XMLHttpRequest();
+            http.open("GET",url);
+            http.send();
+            http.onload=function(){
+                if (http.status == 200){
+                    resolve(this.response);
+                } else reject("Erreur getAllTypeInventaire 1 : "+http.serverResponse);
+            };
+            http.onerror = function(){
+                reject("Erreur getAllTypeInventaire 2 : "+http.serverResponse);
+            };
+        });
+    }
+
+    function getAllTypeObjet() {
+        var url = "./php/get.php?case=all_type_objet";
+        return new Promise(function(resolve,reject){
+            http = new XMLHttpRequest();
+            http.open("GET",url);
+            http.send();
+            http.onload=function(){
+                if (http.status == 200){
+                    resolve(this.response);
+                } else reject("Erreur getAllTypeObjet 1 : "+http.serverResponse);
+            };
+            http.onerror = function(){
+                reject("Erreur getAllTypeObjet 2 : "+http.serverResponse);
             };
         });
     }
--- php/get.php
+++ php/get.php
@@ -3,10 +3,25 @@ include './fonctions.php';
 
 $bdd = generateDb();
 
-if ($_GET['case'] == 'story_line'){
-	$res = getArrayChapitre($bdd);
-} else if ($_GET['case'] == 'personnage'){
+if ($_GET['case'] == 'personnage'){
 	$res = getPersonnage($bdd, intval($_GET['id']));
+} else if ($_GET['case'] == 'all_animal'){
+	$res = getArrayAnimal($bdd);
+} else if ($_GET['case'] == 'all_classe'){
+	$res = getArrayClasse($bdd);
+} else if ($_GET['case'] == 'all_etat'){
+	$res = getArrayEtat($bdd);
+} else if ($_GET['case'] == 'all_genre'){
+	$res = getArrayGenre($bdd);
+} else if ($_GET['case'] == 'all_race'){
+	$res = getArrayRace($bdd);
+} else if ($_GET['case'] == 'all_type_inventaire'){
+	$res = getArrayTypeInventaire($bdd);
+} else if ($_GET['case'] == 'all_type_objet'){
+	$res = getArrayTypeObjet($bdd);
+} else 
+if ($_GET['case'] == 'storyline'){
+	$res = getArrayChapitre($bdd);
 }
 
 echo json_encode($res);
