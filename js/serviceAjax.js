d09637d29648512a29b6bb9f7b4cb4ef0e52d14f
dulymboi <cynthia.dulymbois@etu.u-pec.fr>
2020-04-10 19:56:48 +0200

    getPersonnage dans le service Ajax

--- js/serviceAjax.js
+++ js/serviceAjax.js
@@ -1,6 +1,7 @@
 function makeServiceAjax(){
 	var service = {
-      getStoryLine:getStoryLine  
+      getStoryLine:getStoryLine,
+      getPersonnage:getPersonnage,
     };
 
     function getStoryLine() {
@@ -12,10 +13,27 @@ function makeServiceAjax(){
             http.onload=function(){
                 if (http.status == 200){
                     resolve(this.response);
-                } else reject("Erreur 1 : "+http.serverResponse);
+                } else reject("Erreur getStoryLine 1 : "+http.serverResponse);
             };
             http.onerror = function(){
-                reject("Erreur 2 : "+http.serverResponse);
+                reject("Erreur getStoryLine 2 : "+http.serverResponse);
+            };
+        });
+    }
+
+    function getPersonnage(idPersonnage) {
+        var url = "./php/get.php?case=personnage&id="+idPersonnage;
+        return new Promise(function(resolve,reject){
+            http = new XMLHttpRequest();
+            http.open("GET",url);
+            http.send();
+            http.onload=function(){
+                if (http.status == 200){
+                    resolve(this.response);
+                } else reject("Erreur getPersonnage 1 : "+http.serverResponse);
+            };
+            http.onerror = function(){
+                reject("Erreur getPersonnage 2 : "+http.serverResponse);
             };
         });
     }
--- php/get.php
+++ php/get.php
@@ -5,6 +5,8 @@ $bdd = generateDb();
 
 if ($_GET['case'] == 'story_line'){
 	$res = getArrayChapitre($bdd);
+} else if ($_GET['case'] == 'personnage'){
+	$res = getPersonnage($bdd, intval($_GET['id']));
 }
 
 echo json_encode($res);
