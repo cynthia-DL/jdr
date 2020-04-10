a3397e85db84fd12dbd4aa25075b3c65db9089f8
dulymboi <cynthia.dulymbois@etu.u-pec.fr>
2020-04-10 18:54:27 +0200

    on a un service ajax!

--- js/serviceAjax.js
+++ js/serviceAjax.js
@@ -1,11 +1,10 @@
 function makeServiceAjax(){
-	var urlBase = "/php/";
 	var service = {
       getStoryLine:getStoryLine  
     };
 
     function getStoryLine() {
-        var url = urlBase+"get.php"+offset;
+        var url = "./php/get.php?case=story_line";
         return new Promise(function(resolve,reject){
             http = new XMLHttpRequest();
             http.open("GET",url);
--- php/get.php
+++ php/get.php
@@ -1,3 +1,12 @@
 <?php
+include './fonctions.php';
 
+$bdd = generateDb();
+
+if ($_GET['case'] == 'story_line'){
+	$res = getArrayChapitre($bdd);
+}
+
+echo json_encode($res);
+closeDb($bdd);
 ?>
