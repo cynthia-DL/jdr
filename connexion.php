1a225ae2ec117b89dff03046be8f31ebe1ebf333
dulymboi <cynthia.dulymbois@etu.u-pec.fr>
2020-04-18 13:30:02 +0200

    J'ai ajouter un peu de style a la page de connexion

--- connexion.php
+++ connexion.php
@@ -11,25 +11,28 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-	<form action="verification.php" method="POST">
-		<h1>Connexion</h1>
-		<label>
-			<b>Nom d'utilisateur</b>
-		</label>
-		<input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
-		<label>
-			<b>Mot de passe</b>
-		</label>
-		<input type="password" placeholder="Entrer le mot de passe" name="password" required>
+	<div class="row text-center">
+		<form class="mx-auto col-6" action="verification.php" method="POST">
+		<h1 class="text-primary">Connexion</h1>
+		<br>
+		<div class="form-group">
+	      <label class="text-primary" for="name">Nom d'utilisateur</label>
+	      <input type="text" id="name" class="form-control" placeholder="Entrer le nom d'utilisateur" name="username" required>
+	    </div>
+	    <div class="form-group">
+	      <label class="text-primary" for="password">Mot de passe</label>
+		  <input type="password" id="password" class="form-control" placeholder="Entrer le mot de passe" name="password" required>
+	    </div>
 		<input type="submit" id='submit' value='Connexion'>
 		<?php
 			if(isset($_GET['erreur'])){
 				$err = $_GET['erreur'];
 				if($err==1 || $err==2){
-					echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+					echo "<p class='text-danger'>Utilisateur ou mot de passe incorrect</p>";
 				}
             }
         ?>
     </form>
+	</div>
 </body>
 </html>
